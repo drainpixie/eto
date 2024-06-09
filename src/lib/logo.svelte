2a0e03995c0c610a4553d65a47a3c6b00cbd6360
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-06-10 01:41:06 +0200

    feat: logo component

---
+++ src/lib/logo.svelte
@@ -0,0 +1,15 @@
+<script>
+	let extra = "";
+
+	// 🍝 Best code ever
+	const typewrite = () =>
+		extra.length < 3
+			? (extra += ".")
+			: !extra.endsWith("?")
+				? (extra += "?")
+				: clearInterval(typewriteInterval);
+
+	const typewriteInterval = setInterval(typewrite, 500);
+</script>
+
+<h1 class="highlight">Eto{extra}</h1>
