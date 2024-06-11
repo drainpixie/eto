0085cc791c8ae0a9246c246c2f024d53766512d2
drainpixie <121581793+drainpixie@users.noreply.github.com>
2024-06-11 20:15:37 +0200

    chore: bad string interp

--- flake.nix
+++ flake.nix
@@ -13,18 +13,18 @@
       pkgs = import nixpkgs {inherit system;};
 
       dev = let
-        r = pkgs.redis;
-        t = pkgs.tmux;
+        r = "${pkgs.redis}/bin/redis-";
+        t = "${pkgs.tmux}/bin/tmux";
       in
         pkgs.writeShellScriptBin "dev" ''
-          if ! ${t}/bin/tmux has-session -t dev 2>/dev/null; then
-            ${t}/bin/tmux new-session -d -s dev -n nvim
-            ${t}/bin/tmux new-window -t dev -n vite "pnpm run dev"
-            ${t}/bin/tmux new-window -t dev -n redis "${r}/bin/redis-server --loglevel verbose --appendonly yes"
-            ${t}/bin/tmux new-window -t dev -n redis-cli "${r}/bin/redis-cli"
+          if ! ${t} has-session -t dev 2>/dev/null; then
+            ${t} new-session -d -s dev -n nvim
+            ${t} new-window -t dev -n vite "pnpm run dev"
+            ${t} new-window -t dev -n redis "${r}server --loglevel verbose --appendonly yes"
+            ${t} new-window -t dev -n redis-cli "${r}cli"
           fi
 
-          ${t}/bin/tmux attach-session -t dev
+          ${t} attach-session -t dev
         '';
     in {
       devShells.default = pkgs.mkShell {
